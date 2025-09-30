0159d79de08e45dbfeb9fbda01956ebbd01febc5
ianckruger <ian.kruger.2@gmail.com>
2025-09-30 13:48:58 -0400

    following github repos basic code to see if API works

---
+++ fetch_trains.py
@@ -0,0 +1,11 @@
+from google.transit import gtfs_realtime_pb2
+import requests
+
+
+# Prints out feed travel, stops, arrivals, etc.
+feed = gtfs_realtime_pb2.FeedMessage()
+response = requests.get('https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-ace')
+feed.ParseFromString(response.content)
+for entity in feed.entity:
+    if entity.HasField('trip_update'):
+        print(entity.trip_update)
