0f29f9dfc76e8dd07ec72fcc8fcf4b292b630343
ianckruger <ian.kruger.2@gmail.com>
2026-09-21 22:03:09 -0400

    Forgot to add the workerURL. Map loads properly; Doesnt look as detailed as I would like. Might switch map library

--- frontend/src/components/TransitMap.tsx
+++ frontend/src/components/TransitMap.tsx
@@ -1,8 +1,9 @@
-import {Map} from 'maplibre-gl';
+import {Map, setWorkerUrl} from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import { useEffect, useRef } from 'react';
+import workerUrl from "maplibre-gl/dist/maplibre-gl-worker.mjs?worker&url";
 
-
+setWorkerUrl(workerUrl);
 
 export default function TransitMap() {
     const mapContainer = useRef<HTMLDivElement>(null);
